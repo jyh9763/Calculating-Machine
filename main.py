af8918be51d09d8ed5ac61c554b0933a82db5cbe
jyh9763 <jyh976320@gmail.com>
2024-09-19 01:18:32 +0900

    숫자,연산자 버튼 추가

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from tkinter import Tk
-from tkinter import Button
+from tkinter import *
 
 def alert():
     btn.config(text='1')
@@ -10,13 +9,26 @@ win.geometry('400x500')
 win.title('계산기')
 win.option_add('*Font','맑은고딕')
 
-btn = Button(win)
-btn.config(width=5, height=2)
-btn.config(text='0')
-btn.config(command=alert)
 
+btn = Button(win, text='7', width=10, height=5).grid(row=1, column=0)
+btn = Button(win, text='8', width=10, height=5).grid(row=1, column=1)
+btn = Button(win, text='9', width=10, height=5).grid(row=1, column=2)
+btn = Button(win, text='C', width=10, height=5).grid(row=1, column=3)
 
-btn.pack()
+btn = Button(win, text='4', width=10, height=5).grid(row=2, column=0)
+btn = Button(win, text='5', width=10, height=5).grid(row=2, column=1)
+btn = Button(win, text='6', width=10, height=5).grid(row=2, column=2)
+btn = Button(win, text='+', width=10, height=5).grid(row=2, column=3)
+
+btn = Button(win, text='1', width=10, height=5).grid(row=3, column=0)
+btn = Button(win, text='2', width=10, height=5).grid(row=3, column=1)
+btn = Button(win, text='3', width=10, height=5).grid(row=3, column=2)
+btn = Button(win, text='-', width=10, height=5).grid(row=3, column=3)
+
+btn = Button(win, text='*', width=10, height=5).grid(row=4, column=0)
+btn = Button(win, text='0', width=10, height=5).grid(row=4, column=1)
+btn = Button(win, text='÷', width=10, height=5).grid(row=4, column=2)
+btn = Button(win, text='=', width=10, height=5).grid(row=4, column=3)
 
 
 
